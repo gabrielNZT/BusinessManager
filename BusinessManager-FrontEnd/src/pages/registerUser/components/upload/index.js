13b0d92e127b53c5ee61b3f7017ef9dd60f5b7ac
gabrielNZT <74830051+gabrielNZT@users.noreply.github.com>
2022-10-21 11:08:22 -0300

    handle user photo

--- BusinessManager-FrontEnd/src/pages/registerUser/components/upload/index.js
+++ BusinessManager-FrontEnd/src/pages/registerUser/components/upload/index.js
@@ -3,25 +3,34 @@ import { InboxOutlined } from '@ant-design/icons';
 import ImgCrop from 'antd-img-crop';
 import React, { useState } from 'react';
 
-const INITIAL_VALUE = 0
- 
-const UploadUserPhoto = (props) => {
-    const {formData, handleSetData} = props;
-    const [fileList, setFileList] = useState([]);
-    const onChange = ({ fileList: newFileList }) => {
-        setFileList(newFileList);
-        handleSetData({...formData, [props.item.tag]: newFileList[INITIAL_VALUE]?.originFileObj});
-    };
-    const onPreview = async (file) => {
+const FILE_POSITION = 0;
+const BASE_64 = 1;
+
+const getBase64 = async (file) => file? (await getSRCImage(file)).split('base64,')[BASE_64] : null
+const getSRCImage = async (file) => {
         let src = file.url;
-        if (!src) {
+        if(!src) {
             src = await new Promise((resolve) => {
                 const reader = new FileReader();
                 reader.readAsDataURL(file.originFileObj);
                 reader.onload = () => resolve(reader.result);
-                
-            });
+            })
         }
+        return src
+}
+
+const UploadUserPhoto = (props) => {
+    const { formData, handleSetData } = props;
+    const [fileList, setFileList] = useState([]);
+    const onChange = async ({ fileList: newFileList }) => {
+        setFileList(newFileList);
+        console.log(await getBase64(newFileList[FILE_POSITION]))
+        const base64 = await getBase64(newFileList[FILE_POSITION])
+        handleSetData({ ...formData, [props.item.tag]: base64});
+    };
+    const onPreview = async (file) => {
+        const src = await getSRCImage(file);
+        console.log(src.split('base64,'))
         const image = new Image();
         image.src = src;
         const imgWindow = window.open(src);
@@ -29,7 +38,7 @@ const UploadUserPhoto = (props) => {
     };
     const uploadButton = (
         <div>
-            <InboxOutlined style={{fontSize: '50px'}} />
+            <InboxOutlined style={{ fontSize: '50px' }} />
             <div
                 style={{
                     marginTop: 8,
@@ -43,23 +52,23 @@ const UploadUserPhoto = (props) => {
         <div style={{ display: 'flex', flexDirection: 'column', width: '31.6%', gap: '10px' }}>
             <label> {props.item.label} </label>
             <ImgCrop
-            modalClassName='modal-edit-image'
-            fillColor='black'
-            shape={props.item.shape}
-            modalTitle='Editar a imagem'
-            modalOk='Salvar'
-            modalCancel='Cancelar' 
-            rotate>
+                modalClassName='modal-edit-image'
+                fillColor='black'
+                shape={props.item.shape}
+                modalTitle='Editar a imagem'
+                modalOk='Salvar'
+                modalCancel='Cancelar'
+                rotate>
                 <Upload
                     maxCount={1}
                     showUploadList={true}
                     action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
                     listType="picture-card"
                     fileList={fileList}
-                    onChange={ (file) => onChange(file)}
+                    onChange={(file) => onChange(file)}
                     onPreview={onPreview}
                 >
-                   { uploadButton }
+                    {uploadButton}
                 </Upload>
             </ImgCrop>
         </div>

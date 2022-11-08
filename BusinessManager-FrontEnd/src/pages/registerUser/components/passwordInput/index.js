dadc0afb3432a2f7e0c165a91851024983b8583d
gabriel_nzt <gabrielnunescesarino@gmail.com>
2022-11-08 19:26:04 -0300

    update

--- BusinessManager-FrontEnd/src/pages/registerUser/components/passwordInput/index.js
+++ BusinessManager-FrontEnd/src/pages/registerUser/components/passwordInput/index.js
@@ -1,14 +1,27 @@
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import { Input, Space } from 'antd';
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function PasswordField(props) {
-    const { formData } = props
+    const { formData, handleSetData } = props
+    const [disabled, setDisabled] = useState(!formData?.passwordLocked)
+
+    useEffect(() => {
+        const { password, repeatPassword, ...rest } = formData
+        if (!formData.passwordLocked) {
+            handleSetData({ ...rest })
+            setDisabled(true)
+        } else setDisabled(false)
+    }, [formData, handleSetData]);
+
     return (
         <div style={{ display: 'flex', flexDirection: 'column', width: '31.6%', gap: '10px' }}>
             <label> {props.item.label} </label>
             <Space direction="vertical" className='password-field'>
                 <Input.Password
+                    disabled={disabled}
                     value={formData[props.item.tag]}
                     autoComplete='false'
                     onChange={(event) => props.handleSetData({ ...formData, [props.item.tag]: event.target.value })}
